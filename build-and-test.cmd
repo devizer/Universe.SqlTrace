e631debf68ebe6bda0b9c3b06f618800963b88e0
devizer <devizer@users.github.com>
2018-05-13 20:42:44 +0300

    New Modern Build tools discovery

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,7 +1,11 @@
+type Prepare-NuGet-And-build-tools.ps1 | powershell -c -
+call ~local-build-tools.cmd
+
 pushd include\Universe.SqlServerJam\src
 set TEST_SQL_NET_DURATION_OF_Ping=42
 set TEST_SQL_NET_DURATION_OF_Upload=42
 set TEST_SQL_NET_DURATION_OF_Download=42
+"%NUGET_EXE%" restore
 call Modern-Build-and-Test.cmd
 popd
 
@@ -9,5 +13,5 @@ echo START TRACE
 call build-only.cmd
 set work=Universe.SqlTrace.Tests\bin\Debug\
 pushd "%work%"
-..\..\..\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe --workers=1 Universe.SqlTrace.Tests.exe
+"%NUNIT_RUNNER_EXE%" --workers=1 Universe.SqlTrace.Tests.exe
 popd
--- build-only.cmd
+++ build-only.cmd
@@ -3,7 +3,8 @@ echo [assembly: System.Reflection.AssemblyVersion("%SqlTrace_Version%.0")]     >
 echo [assembly: System.Reflection.AssemblyFileVersion("%SqlTrace_Version%.0")] >> Universe.SqlTrace\Properties\AssemblyVersion.cs 
 
 rem *** BUILD ***
-call Prepare-Nuget-and-Build-Tools.cmd
-"%BUILD_TOOLS_ONLINE%\nuget.exe" restore
-"%MS_BUILD_2017%" Universe.SqlTrace.sln /t:Rebuild /v:m /p:Configuration=Debug
-"%MS_BUILD_2017%" Universe.SqlTrace.sln /t:Rebuild /v:m /p:Configuration=Release
+type Prepare-NuGet-And-build-tools.ps1 | powershell -c -
+call ~local-build-tools.cmd
+"%NUGET_EXE%" restore
+"%MSBUILD_EXE%" Universe.SqlTrace.sln /t:Rebuild /v:m /p:Configuration=Debug
+"%MSBUILD_EXE%" Universe.SqlTrace.sln /t:Rebuild /v:m /p:Configuration=Release
