ced6bca7bd67ba6af0326db80382eb5e024301f1
devizer <devizer@users.github.com>
2019-10-22 04:09:54 +0300

    fix missed merge

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,3 +1,5 @@
+@rem git clone --recurse-submodules https://github.com/devizer/Universe.SqlTrace
+
 type Prepare-NuGet-And-build-tools.ps1 | powershell -c -
 call ~local-build-tools.cmd
 "%NUGET_EXE%" restore
---
+++ clone-buid-and-test.cmd
@@ -0,0 +1,7 @@
+mkdir C:\Lab >/nul 2>&1
+pushd C:\Lab
+rd /q /s Universe.SqlTrace
+git clone --recurse-submodules https://github.com/devizer/Universe.SqlTrace
+cd Universe.SqlTrace
+call build-and-test.cmd 2>&1 | tee log.log
+popd
