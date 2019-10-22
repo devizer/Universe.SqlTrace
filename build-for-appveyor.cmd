21967f54236ec17f0404b0d0a3b5bd583116c993
devizer <devizer@users.github.com>
2019-10-22 14:50:59 +0300

    build-for-appveyor.cmd 3

--- build-for-appveyor.cmd
+++ build-for-appveyor.cmd
@@ -4,5 +4,9 @@ type Prepare-NuGet-And-build-tools.ps1 | powershell -c -
 call ~local-build-tools.cmd
 "%NUGET_EXE%" restore
 
+pushd include\Universe.SqlServerJam\src
+"%NUGET_EXE%" restore
+popd
+
 call build-only.cmd
 
