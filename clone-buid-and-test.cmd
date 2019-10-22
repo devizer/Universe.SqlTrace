ca7520125656679fed443ed9a688e85758bcbf02
devizer <devizer@users.github.com>
2019-10-22 17:00:40 +0300

    test for Jam on appveyor

--- clone-buid-and-test.cmd
+++ clone-buid-and-test.cmd
@@ -3,5 +3,5 @@ pushd C:\Lab
 rd /q /s Universe.SqlTrace
 git clone --recurse-submodules https://github.com/devizer/Universe.SqlTrace
 cd Universe.SqlTrace
-call build-and-test.cmd 2>&1 | tee log.log
+call build-for-appveyor.cmd 2>&1 | tee log.log
 popd
