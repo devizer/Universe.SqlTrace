024d07416f61e63b08a9e075ac3124f2c8abc466
devizer <devizer@users.github.com>
2019-10-24 09:53:11 +0300

    store logs for old SQL on old Image

---
+++ Info/Store-Logs-2.cmd
@@ -0,0 +1,2 @@
+rm -rf C:\projects\universe-sqltrace\Artifact\*
+powershell -f Store-Logs-2.ps1
--- Info/Store-Logs-2.ps1
+++ Info/Store-Logs-2.ps1
@@ -9,20 +9,16 @@
         [System.String]::Concat("[", (new-object System.DateTime(0)).AddMilliseconds($Global:startAt.ElapsedMilliseconds).ToString("HH:mm:ss"), "]");
     }; $_=Get-Elapsed;
 
-    $Env:ARTIFACT = "V:\Artifact"
+    $Env:ARTIFACT = "C:\projects\universe-sqltrace\Artifact"
     
     Say "Store All The Logs"
     Get-ChildItem -Path "C:\" -force -directory | % { $folder=$_;
         Say "Store logs for $($folder.FullName)"
-        $accessDenied = "$Env:ARTIFACT\Access Denied ($($folder.Name)).txt"
-        # $files=(Get-ChildItem -Path "$($folder.FullName)" -recurse -force -file -Include "*.log" *> $accessDenied)
-        $files=(Get-ChildItem -Path "C:\Apps\Palo-Alto-Networks\GlobalProtect" -recurse -force -file -Include "*.log" 2> $accessDenied)
-        $files
-        $files.Count
+        $files=(Get-ChildItem -Path $folder.FullName -recurse -force -file -Include "*.log" 2> "$Env:ARTIFACT\Access Denied ($($folder.Name)).txt")
         if ( @($files).Count -gt 0 ) {
             Say "Pack $(@($files).Count) log files from $($folder.FullName)"
-            @($files) | % { $_.FullName } | sort -unique | % { $_.substring("$($folder.FullName)".Length) } | Out-File "$Env:ARTIFACT\log-files-$($folder.Name).list" utf8
-            pushd "$($folder.FullName)"
+            @($files) | % { $_.FullName } | sort -unique | % { $_.substring(3) } | Out-File "$Env:ARTIFACT\log-files-$($folder.Name).list" utf8
+            pushd "C:\"
             7z a -t7z -mx=3 -ms=on -spf "$Env:ARTIFACT\All The Logs for $($folder.Name).7z" "@$($Env:ARTIFACT)\log-files-$($folder.Name).list"
             popd
         }
