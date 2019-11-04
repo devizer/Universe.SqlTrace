3d7cf5495187f8db5b20a037bbe6af0f82555686
devizer <devizer@users.github.com>
2019-11-04 05:38:15 +0200

    measure Disk IO Benchmark RC2

--- appveyor/run-DiskSpd-tests.cmd
+++ appveyor/run-DiskSpd-tests.cmd
@@ -1,8 +1,8 @@
 mkdir ..\ARTIFACT 1>nul 2>&1
-diskspd.exe -d15 -c6000M -b1M -Sw -Su -t1 -w0 -s1b "io-perf.tmp" 
-diskspd.exe -d15 -c6000M -b1M -Sw -Su -t1 -w100 -s1b "io-perf.tmp" 
+diskspd.exe -d15 -c11000M -b1M -Sw -Su -t1 -w0 -s1b "io-perf.tmp" 
+diskspd.exe -d15 -c11000M -b1M -Sw -Su -t1 -w100 -s1b "io-perf.tmp" 
 
-diskspd.exe -d15 -c6000M -b4K -Sw -Su -t8 -w0 -r1b "io-perf.tmp" 
-diskspd.exe -d15 -c6000M -b4K -Sw -Su -t8 -w100 -r1b "io-perf.tmp" 
+diskspd.exe -d15 -c11000M -b4K -Sw -Su -t8 -w0 -r1b "io-perf.tmp" 
+diskspd.exe -d15 -c11000M -b4K -Sw -Su -t8 -w100 -r1b "io-perf.tmp" 
 
 del "io-perf.tmp"
