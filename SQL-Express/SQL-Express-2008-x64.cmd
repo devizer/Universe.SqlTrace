796ebcecd4a06369399c3920953a1251fddd77ae
devizer <devizer@users.github.com>
2019-11-06 00:37:40 +0200

    [skip appveyor] skipping .circleci/*

--- SQL-Express/SQL-Express-2008-x64.cmd
+++
@@ -1,1 +0,0 @@
-type Install-SQL-2008-x64.ps1 | powershell -c -
--- SQL-Express/list-sql-services.ps1
+++
@@ -1,1 +0,0 @@
-get-wmiobject win32_service | where {$_.Name.ToLower().IndexOf("sql") -ge 0 } | sort-object -Property "DisplayName" | ft Name, DisplayName, StartMode, State
