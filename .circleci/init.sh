9eabee0642e021f476a2f6145eac85b97e193c0c
devizer <devizer@users.github.com>
2019-11-06 02:12:55 +0200

    refactoring

---
+++ .circleci/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+git config --global core.autocrlf false
+
+echo Configure apt
+echo 'Acquire::Check-Valid-Until "0";' | sudo tee /etc/apt/apt.conf.d/10no--check-valid-until 
+echo 'APT::Get::Assume-Yes "true";' | sudo tee /etc/apt/apt.conf.d/11assume-yes               
+echo 'APT::Get::AllowUnauthenticated "true";' | sudo tee /etc/apt/apt.conf.d/12allow-unauth   
+
+sudo apt -qq update >/dev/null ; sudo apt install -y -qq git sudo jq tar bzip2 gzip curl lsb-release procps
