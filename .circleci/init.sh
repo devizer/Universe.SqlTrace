19b8a39e9dd9eeff2458c1628c5ec2b49e9c0802
devizer <devizer@users.github.com>
2019-11-06 02:16:12 +0200

    refactoring 3

--- .circleci/init.sh
+++ .circleci/init.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-git config --global core.autocrlf false
-
 echo Configure apt
 echo 'Acquire::Check-Valid-Until "0";' | sudo tee /etc/apt/apt.conf.d/10no--check-valid-until 
 echo 'APT::Get::Assume-Yes "true";' | sudo tee /etc/apt/apt.conf.d/11assume-yes               
