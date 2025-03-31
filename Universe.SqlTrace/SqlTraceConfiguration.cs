20f115325be901840389265ac73a3b33215ad93b
devizer <devizer@github.io>
2025-04-01 00:23:44 +0300

    Default is System Sql Client, updated nuspec

--- Universe.SqlTrace/SqlTraceConfiguration.cs
+++ Universe.SqlTrace/SqlTraceConfiguration.cs
@@ -18,14 +18,16 @@ namespace Universe.SqlTrace
         // TODO: Remove this type and move ALL THE METHOD to private instance methods of SqlTraceReader
         public static DbParameter CreateCommandParameter(this DbConnection connection, string parameterName, object parameterValue)
         {
-            DbParameter p = SqlTraceConfiguration.DbProvider.CreateParameter();
+            var dbProviderFactory = SqlTraceConfiguration.DbProvider ?? SqlClientFactory.Instance;
+            DbParameter p = dbProviderFactory.CreateParameter();
             p.ParameterName = parameterName;
             p.Value = parameterValue;
             return p;
         }
         public static void AddCommandParameter(this DbCommand command, string parameterName, DbType parameterType, object parameterValue)
         {
-            var p = SqlTraceConfiguration.DbProvider.CreateParameter();
+            var dbProviderFactory = SqlTraceConfiguration.DbProvider ?? SqlClientFactory.Instance;
+            var p = dbProviderFactory.CreateParameter();
             p.DbType = parameterType;
             p.ParameterName = parameterName;
             p.Value = parameterValue;
@@ -33,14 +35,16 @@ namespace Universe.SqlTrace
         }
         public static DbConnection CreateConnection(string connectionString)
         {
-            DbConnection ret = SqlTraceConfiguration.DbProvider.CreateConnection();
+            var dbProviderFactory = SqlTraceConfiguration.DbProvider ?? SqlClientFactory.Instance;
+            DbConnection ret = dbProviderFactory.CreateConnection();
             ret.ConnectionString = connectionString;
             return ret;
         }
 
         public static DbCommand CreateDbCommand(this DbConnection connection)
         {
-            var ret = SqlTraceConfiguration.DbProvider.CreateCommand();
+            var dbProviderFactory = SqlTraceConfiguration.DbProvider ?? SqlClientFactory.Instance;
+            var ret = dbProviderFactory.CreateCommand();
             ret.Connection = connection;
             return ret;
         }
--- set-version.cmd
+++ set-version.cmd
@@ -1,2 +1,2 @@
-set SqlTrace_Version=1.8
+set SqlTrace_Version=1.8.2
 set PKG_VERSION=%SqlTrace_Version%
